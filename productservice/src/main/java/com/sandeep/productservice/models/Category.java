3b0fa6180435cdb90a18742eece43a6485cfd502
Sandeep Singh <sandeepgaurdec13@gmail.com>
2024-04-07 13:41:52 +0530

    Connected Mysql DB for Product-Service

--- productservice/src/main/java/com/sandeep/productservice/models/Category.java
+++ productservice/src/main/java/com/sandeep/productservice/models/Category.java
@@ -1,5 +1,8 @@
 package com.sandeep.productservice.models;
 
+import jakarta.persistence.Entity;
+
+@Entity(name="categories")
 public class Category extends BaseModel{
     private String name;
 }
--- productservice/src/main/java/com/sandeep/productservice/models/Product.java
+++ productservice/src/main/java/com/sandeep/productservice/models/Product.java
@@ -1,14 +1,21 @@
 package com.sandeep.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
+    //P:C
+    //1:1
+    //M:1
+    @ManyToOne
     private Category category;
     private Double price;
 }
